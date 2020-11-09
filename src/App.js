0d41a6d1494c5e63ab9cc4fcb286a336577088c1
Alex Partida <60553154+alpartida@users.noreply.github.com>
2020-11-08 21:45:24 -0800

    added css

--- src/App.js
+++ src/App.js
@@ -4,61 +4,63 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-     <div class="container">
-      <div class="city-form">
-        <form>
-          <input type="text" placeholder="city" />
-          <button type="submit" id="submitButton" class="btn btn-secondary">
-            Submit
-          </button>
-          <button id="localButton" class="btn btn-light">
-            Current Location
-          </button>
-        </form>
-      </div>
-      <div class="current-weather">
-        <h3></h3>
-        <h1>Search For A City!</h1>
-        <h2>
-          <span id="temp"></span>
-          <span class="units"><a href="#">C</a> | <a href="#">F</a> </span>
-        </h2>
-        <div id="date"></div>
-        <img id="mainWeatherImage" class="mainWeatherImg" src="" />
-      </div>
-      <div class="days">
-        <div class="day-1">
-          <p>today</p>
+      <div class="container">
+        <div class="city-form">
+          <form>
+            <input type="text" placeholder="city" />
+            <button type="submit" id="submitButton" class="btn btn-secondary">
+              Submit
+            </button>
+            <button id="localButton" class="btn btn-light">
+              Current Location
+            </button>
+          </form>
         </div>
-        <div class="day-2">
-          <p>tomorrow</p>
+        <div class="current-weather">
+          <h3></h3>
+          <h1>Search For A City!</h1>
+          <h2>
+            <span id="temp"></span>
+            <span class="units">
+              <a href="#">C</a> | <a href="#">F</a>{" "}
+            </span>
+          </h2>
+          <div id="date"></div>
+          <img id="mainWeatherImage" class="mainWeatherImg" src="" />
         </div>
-      </div>
-      <div class="forecast">
-        <div class="row">
-          <div class="col">
-            <h4>now</h4>
-            <p>60°</p>
-            <i class="fas fa-sun"></i>
+        <div class="days">
+          <div class="day-1">
+            <p>today</p>
           </div>
-          <div class="col">
-            <h4>noon</h4>
-            <p>60°</p>
-            <i class="fas fa-cloud-sun"></i>
-          </div>
-          <div class="col">
-            <h4>4pm</h4>
-            <p>60°</p>
-            <i class="fas fa-cloud-sun"></i>
-          </div>
-          <div class="col">
-            <h5>tonight</h5>
-            <p>60°</p>
-            <i class="fas fa-cloud-moon-rain"></i>
+          <div class="day-2">
+            <p>tomorrow</p>
           </div>
         </div>
+        <div class="forecast">
+          <div class="row">
+            <div class="col">
+              <h4>now</h4>
+              <p>60°</p>
+              <i class="fas fa-sun"></i>
+            </div>
+            <div class="col">
+              <h4>noon</h4>
+              <p>60°</p>
+              <i class="fas fa-cloud-sun"></i>
+            </div>
+            <div class="col">
+              <h4>4pm</h4>
+              <p>60°</p>
+              <i class="fas fa-cloud-sun"></i>
+            </div>
+            <div class="col">
+              <h5>tonight</h5>
+              <p>60°</p>
+              <i class="fas fa-cloud-moon-rain"></i>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
